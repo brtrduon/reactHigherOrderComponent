126c73d59c2c56b15e8b5c124a951ef9e58fdd36
unknown <you@example.com>
2018-02-19 17:13:27 -0800

    added stuff

---
+++ src/components/resources.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export default () => {
+    return (
+        <div>
+            Crack
+            <ul>
+                <li>Cocaine</li>
+                <li>Baking soda</li>
+                <li>Lighter</li>
+                <li>Spoon</li>
+            </ul>
+        </div>
+    );
+}
