d22934cf35e50f8c6696a257096bf1d0b7c5e67d
unknown <you@example.com>
2018-02-19 17:38:19 -0800

    authentication works

--- src/components/require_authentication.js
+++ src/components/require_authentication.js
@@ -3,8 +3,25 @@ import { connect } from 'react-redux';
 
 export default function(ComposedComponent) {
     class Authentication extends Component {
+        static contextTypes = {
+            router: React.PropTypes.object
+        }
+        // 'context' is similar to props but allows us to skip levels of component hierarchy
+        // react forces us to define 'context' in the syntax as seen above
+
+        componentWillMount() {
+            if (!this.props.authenticated) {
+                this.context.router.push('/');
+            }
+        }
+
+        componentWillUpdate(nextProps) {
+            if(!nextProps.authenticated) {
+                this.context.router.push('/');
+            }
+        }
+
         render() {
-            console.log(this.props.authenticated);
             return <ComposedComponent {...this.props} />
         }
     }
@@ -16,3 +33,4 @@ export default function(ComposedComponent) {
     return connect(mapStateToProps)(Authentication);
 }
 
+// so it is basically best practice to handle authentication in a separate file/component rather than handle authentication in some file with pre-existing code(?)
